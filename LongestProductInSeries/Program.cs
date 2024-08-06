cf115470c818809c6d810def007e42d3fe78171f
Maşite <masite_bayram@hotmail.com>
2024-08-06 19:15:54 +0300

    Special Pythagorean Triplet

---
+++ LongestProductInSeries/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LongestProductInSeries;
+
+Solution solution = new Solution();
+long result = solution.FingLongestProduct(13);
+Console.WriteLine("Hello, World!");
---
+++ Readme.md
@@ -0,0 +1,6 @@
+I'll share my answers for Project Euler problems.
+https://projecteuler.net
+
+If you want to be my friend, you can use my key
+My Own Key:
+2169218_0J3mHMjK0BGu4j6ZDUYhANvFc0xDibkQ
