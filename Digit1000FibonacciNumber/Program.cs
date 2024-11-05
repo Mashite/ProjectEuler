33131ff8ad93ad059dcf4eb77c07e3694feb6fda
Maşite <masite_bayram@hotmail.com>
2024-11-05 20:57:27 +0300

    Digit 1000 Fibonacci Number

---
+++ Digit1000FibonacciNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿using Digit1000FibonacciNumber;
+using System.Numerics;
+
+Solution solution = new Solution();
+BigInteger result = solution.Solve(100);
+Console.WriteLine(result.ToString());
---
+++ Digit1000FibonacciNumber/Solution.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Numerics;
+
+namespace Digit1000FibonacciNumber
+{
+    public class Solution
+    {
+        public BigInteger Solve(BigInteger f)
+        {
+            BigInteger f1 = 1;
+            BigInteger f2 = 1;
+            BigInteger f3=0;
+            int i = 2;
+            while(f3.ToString().Length<1000)
+            {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+                i++;
+            }
+
+            return i;
+        }
+        public long Fibonacci(long f)
+        {
+            if (f == 1 || f==2)
+                return 1;
+            return Fibonacci(f-1) + Fibonacci(f - 2);
+        }
+    }
+}
